481d7ff149e386ab083f9d63223bbe0263a029df
乔仁杰 <395505011@qq.com>
2018-05-16 21:23:35 +0800

    搭建项目Maven框架，准备开始项目

---
+++ Practices/src/main/java/com/java/App.java
@@ -0,0 +1,14 @@
+package com.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "We can start the work now" );
+    }
+}
